3bb8a677a8c6a232d7921813c9e4450e58e9dfd8
Shashank Parekh <shashank.parekh@go-mmt.com>
2018-10-09 14:47:55 +0530

    Added script.

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
-dfisdf TEST1123123123sadf dfasdf
- 
+#!/bin/bash
+
+pip install flake8
+tag=$(git diff -U0 | flake8 --diff - | wc -l)
+
+if [ $tag >= $1 ]
+then
+  output=0
+else
+  output=-1
+fi
+
+exit $output
